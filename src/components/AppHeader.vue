a736fb4091fe3558b434e255956ea84a307cee6a
Andrea030105 <tummoloandrea05@gmail.com>
2026-04-16 15:41:00 +0200

    Header-complete-for-only-pages

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,33 @@
 <script>
-export default {};
+export default {
+  data() {
+    return {};
+  },
+};
 </script>
 <template lang="">
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <router-link class="navbar-brand" :to="{ name: 'home' }">BnB</router-link>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <nav class="navbar navbar-expand-lg navbar-header">
+    <div class="container-fluid text-center">
+      <router-link class="navbar-brand logo" :to="{ name: 'home' }">
+        <img src="../assets/images/loghi/Logo_Completo.svg" alt="Logo" />
+      </router-link>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div
+        class="collapse navbar-collapse flex-grow-0"
+        id="navbarSupportedContent"
+      >
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <router-link class="nav-link" :to="{ name: 'home' }"
@@ -23,8 +45,43 @@ export default {};
             >
           </li>
         </ul>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item m-1">
+            <a
+              class="btn btn-small btn-login"
+              href="http://127.0.0.1:8000/login"
+              >Login</a
+            >
+          </li>
+          <li class="nav-item m-1">
+            <a
+              class="btn btn-small btn-login"
+              href="http://127.0.0.1:8000/register"
+              >Registrati</a
+            >
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
 </template>
-<style lang=""></style>
+<style lang="scss">
+@use "../styles/partial/variables" as *;
+.navbar-header {
+  background-color: $clGray;
+  color: $clBlack;
+  font-weight: bolder;
+  .logo {
+    width: 100px;
+  }
+
+  .btn-login {
+    background-color: $clRed !important;
+    color: $clWhite !important;
+
+    &:hover {
+      background-color: $clRedHover !important;
+    }
+  }
+}
+</style>
