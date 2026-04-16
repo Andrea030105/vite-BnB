5a397e1a3b6e80c9f94810993888a2c40e432d0b
Andrea030105 <tummoloandrea05@gmail.com>
2026-04-16 18:27:33 +0200

    created-apartment-cards-and-fetched-data-from-backoffice-via-axios

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import { router } from "./router";
 import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 createApp(App).use(router).mount("#app");
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,5 +1,25 @@
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      apartments: [],
+      baseUrl: "http://127.0.0.1:8000",
+    };
+  },
+  methods: {
+    getApartments() {
+      axios.get(`${this.baseUrl}/api/apartments`).then((response) => {
+        console.log(response.data);
+        this.apartments = response.data.apartments;
+      });
+    },
+  },
+  mounted() {
+    this.getApartments();
+  },
+};
 </script>
 <template lang="">
   <div class="container-fluid">
@@ -30,24 +50,39 @@ export default {};
       </div>
     </div>
     <div class="row">
-      <div class="col">
-        <div class="card" style="width: 18rem">
-          <img src="../assets/images/apartment/villa.Jpeg" alt="Villa" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <div>
+      <div class="col d-flex gap-5 flex-wrap">
+        <div v-for="(apartment, index) in apartments" :key="index">
+          <div class="card" style="width: 18rem">
+            <img :src="`${baseUrl}/storage/${apartment.image}`" alt="Villa" />
+            <div class="card-body">
+              <h5 class="card-title">{{ apartment.title }}</h5>
               <ul class="list-unstyled">
                 <div class="my-2">
                   <label><strong>Metri Quadrati:</strong></label>
-                  <li class="badge mx-1">3</li>
+                  <li class="badge mx-1">{{ apartment.square_meters }}</li>
                 </div>
                 <div class="my-2">
-                  <label><strong>Metri Quadrati:</strong></label>
-                  <li class="badge mx-1">3</li>
+                  <label><strong>Stanze:</strong></label>
+                  <li class="badge mx-1">{{ apartment.rooms }}</li>
+                </div>
+                <div class="my-2">
+                  <label><strong>Camera/e da letto:</strong></label>
+                  <li class="badge mx-1">{{ apartment.bedrooms }}</li>
+                </div>
+                <div class="my-2">
+                  <label><strong>Bagni:</strong></label>
+                  <li class="badge mx-1">{{ apartment.bathrooms }}</li>
                 </div>
               </ul>
+              <h5>Prezzo a notte:</h5>
+              <div>
+                <p class="badge mx-1">
+                  {{ apartment.price_per_night }}
+                  <i class="fa-solid fa-euro-sign"></i>
+                </p>
+              </div>
+              <a href="#" class="btn btn-small btn-principal">Go somewhere</a>
             </div>
-            <a href="#" class="btn btn-small btn-principal">Go somewhere</a>
           </div>
         </div>
       </div>
