a476ab3dbcbb51c07f0dec03ffbd7836a8c25ae8
Andrea030105 <tummoloandrea05@gmail.com>
2026-04-22 16:48:42 +0200

    handle-no-results-in-advanced-search

--- src/pages/AppAdvancedSearch.vue
+++ src/pages/AppAdvancedSearch.vue
@@ -15,10 +15,12 @@ export default {
       bathrooms: "",
       squareMeters: "",
       price: "",
+      hasFiltered: false,
     };
   },
   methods: {
     getApartmentFilter() {
+      this.hasFiltered = true;
       this.filterApartment = store.apartments.filter((element) => {
         return (
           (!this.address ||
@@ -145,6 +147,16 @@ export default {
             </div>
           </div>
         </div>
+
+        <div
+          class="col-12 col-lg-9 d-flex align-items-center justify-content-center"
+          v-else-if="hasFiltered"
+        >
+          <div class="row text-danger">
+            <h2><i class="fa-solid fa-x"></i> Nessun Risultato trovato</h2>
+          </div>
+        </div>
+
         <!-- RISULTATI NON FILTRATI -->
         <div class="col-12 col-lg-9" v-else>
           <div class="row row-cols-1 row-cols-md-2 row-cols-xl-4 g-4">
